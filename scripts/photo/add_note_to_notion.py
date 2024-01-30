e1aa9627d32acb49cb8ae157436fe2df4548b617
Miki Saarna <86010458+miki-saarna@users.noreply.github.com>
2024-01-29 19:18:03 -0500

    Converted curl to requests

--- scripts/photo/add_note_to_notion.py
+++ scripts/photo/add_note_to_notion.py
@@ -1,10 +1,11 @@
-import subprocess
+# import subprocess
 from dotenv import load_dotenv
 from scan_image import ScanImageWithChatGPT
 from cli_chatgpt import start_interactive_chat
 import os
 load_dotenv()
 from openai import OpenAI
+import requests
 
 client = OpenAI()
 NOTION_API_KEY = os.environ["NOTION_API_KEY"]
@@ -12,7 +13,7 @@ NOTION_API_KEY = os.environ["NOTION_API_KEY"]
 class AddNoteToNotion:
   def __init__(self, image_file_path, PAGE_ID):
     image_text = ScanImageWithChatGPT(image_file_path).image_text
-    prompt = f"Using the `gpt4_vision` model, I deciphered a handwritten message on a sticky note I have. I would like to proceed by recording the message to a note-taking platform. Since this is a hand-written note on a small sticky note, can you give me 3 recommendations on the final version of the message I should use? In your recommendations, please ensure you fix any and all grammatical or sentence structure errors you find. However, do not alter the core content of the message; you can only paraphrase it to the best of your ability. After I select a suitable recommendation, please repeat the selected recommendation verbatim - the reason for repeating is for easy retrieval of the value (DO NOT prefix/suffix the recommendation. Simply repeat the recommendation and only the recommendation). Here is the message: {image_text}. Here is addiitonal contextual background that may be helpful to you regarding this message: {self.context}"
+    prompt = f"Using the `gpt4_vision` model, I deciphered a handwritten message on a sticky note I have. I would like to proceed by recording the message to a note-taking platform. Since this is a hand-written note on a small sticky note, can you give me 3 recommendations on the final version of the message I should use? In your recommendations, please ensure you fix any and all grammatical or sentence structure errors you find. However, do not alter the core content of the message; you can only paraphrase it to the best of your ability. After I select a suitable recommendation, please repeat the selected recommendation verbatim - the reason for repeating is for easy retrieval of the value (DO NOT prefix/suffix the recommendation. Simply repeat the recommendation and only the recommendation. Additionally, when repeating the recommendation, ensure that it isn't wrapped with quotations). Here is the message: {image_text}. Here is addiitonal contextual background that may be helpful to you regarding this message: {self.context}"
 
     start_interactive_chat(initial_text=prompt, on_chat_end=self.upload_note_to_notion)
 
@@ -21,34 +22,71 @@ class AddNoteToNotion:
 
   def upload_note_to_notion(self, message):
     self.recommended_text = message.content
-    testText = self.genCurlCmd()
-    subprocess.run(self.genCurlCmd(), shell=True)
+    # subprocess.run(self.genCurlCmd(), shell=True)
 
-  def genCurlCmd(self):
-    return f"""
-      curl -X PATCH 'https://api.notion.com/v1/blocks/{PAGE_ID}/children' \
-        -H 'Authorization: Bearer {NOTION_API_KEY}' \
-        -H "Content-Type: application/json" \
-        -H "Notion-Version: 2022-06-28" \
-        --data '{{
-          "children": [
-              {{
-                  "object": "block",
-                  "type": "paragraph",
-                  "paragraph": {{
-                      "rich_text": [
-                          {{
-                              "type": "text",
-                              "text": {{
-                                  "content": {self.recommended_text}
-                              }}
-                          }}
-                      ]
-                  }}
-              }}
-          ]
-      }}'
-      """
+    url = f'https://api.notion.com/v1/blocks/{PAGE_ID}/children'
+    headers = {
+      'Authorization': f'Bearer {NOTION_API_KEY}',
+      'Content-Type': 'application/json',
+      'Notion-Version': '2022-06-28'
+    }
+    data = {
+       "children": [
+            {
+                "object": "block",
+                "type": "paragraph",
+                "paragraph": {
+                    "rich_text": [
+                        {
+                            "type": "text",
+                            "text": {
+                                "content": self.remove_quotations(self.recommended_text)
+                            }
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+
+    response = requests.patch(url, headers=headers, json=data)
+
+    if response.status_code == 200:
+      print("Patch request successful")
+    else:
+      print(f"Patch request failed with status code: {response.status_code}")
+
+  @staticmethod
+  def remove_quotations(str):
+    if str.startswith('"') and str.endswith('"'):
+        return str[1:-1]
+    return str
+
+  # def genCurlCmd(self):
+  #   return f"""
+  #     curl -X PATCH 'https://api.notion.com/v1/blocks/{PAGE_ID}/children' \
+  #       -H 'Authorization: Bearer {NOTION_API_KEY}' \
+  #       -H "Content-Type: application/json" \
+  #       -H "Notion-Version: 2022-06-28" \
+  #       --data '{
+  #         "children": [
+  #             {{
+  #                 "object": "block",
+  #                 "type": "paragraph",
+  #                 "paragraph": {{
+  #                     "rich_text": [
+  #                         {{
+  #                             "type": "text",
+  #                             "text": {{
+  #                                 "content": f'"{self.remove_quotations(self.recommended_text)}"'
+  #                             }}
+  #                         }}
+  #                     ]
+  #                 }}
+  #             }}
+  #         ]
+  #     }'
+  #     """
 
 if __name__ == "__main__":
     image_file_path = "/Users/mikitosaarna/Projects/notion/scan-photo-to-note/public/photos/note-1.jpeg"
